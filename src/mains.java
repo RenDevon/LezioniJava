78eff97ed17791861c8ad2ab55601f371001f636
RenDevon <amnesiaselettiva.mt@gmail.com>
2023-04-04 00:05:49 +0200

    impapucchiato con lista

--- src/mains.java
+++ src/mains.java
@@ -1,18 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class mains {
 
     public static void main(String[] args) {
-    String occhio = ";scemo;beppe;scemo;enrico;mel;ytyty;";
-    occhio = occhio.substring(1);
-    String[] arreyDiStringhe = occhio.split(";");
+        String occhio = ";scemo;beppe;scemo;enrico;mel;ytyty;";
+        occhio = occhio.substring(1);
+        List<String> arreyDiStringhe = Arrays.asList(occhio.split(";"));
+        List<String> nuovoArray = new ArrayList<>(arreyDiStringhe);
+        nuovoArray.add("genoveffa");
+//        System.out.println(arreyDiStringhe.size());
+        String variabileDappoggio = "";
+        for (int i = 0; i < nuovoArray.size(); i++) {
 
-        System.out.println(arreyDiStringhe.length);
-    for(int i = 0; i < arreyDiStringhe.length; i++ ) {
+            for (int x = 0; x < nuovoArray.size(); x++) {
 
+                if (nuovoArray.get(i).equals( nuovoArray.get(x))) {
+                    System.out.println("minchia, è uguale!");
+                }
+                System.out.println(nuovoArray.get(i));
+            }
+            ;
 
-        System.out.println(arreyDiStringhe[i]);
-    }
+//            variabileDappoggio = arreyDiStringhe.get(i);
+//            if(variabileDappoggio == arreyDiStringhe.get(i + 1)){
+//
+//            }
 
-
+        }
 
 
     }

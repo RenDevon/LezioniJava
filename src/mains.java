d33a3efe4718161d8125c5971a148ef9b8514eea
RenDevon <amnesiaselettiva.mt@gmail.com>
2023-04-04 00:05:49 +0200

    impapucchiato con lista

--- src/mains.java
+++ src/mains.java
@@ -1,18 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class mains {
 
     public static void main(String[] args) {
-    String occhio = ";scemo;beppe;scemo;enrico;mel;ytyty;";
-    occhio = occhio.substring(1);
-    String[] arreyDiStringhe = occhio.split(";");
+        String occhio = ";scemo;beppe;scemo;enrico;mel;ytyty;";
+        occhio = occhio.substring(1);
+        List<String> arreyDiStringhe = Arrays.asList(occhio.split(";"));
 
-        System.out.println(arreyDiStringhe.length);
-    for(int i = 0; i < arreyDiStringhe.length; i++ ) {
+//        System.out.println(arreyDiStringhe.size());
+        String variabileDappoggio = "";
+        for (int i = 0; i < arreyDiStringhe.size(); i++) {
 
+            for (int x = 0; x < arreyDiStringhe.size(); x++) {
 
-        System.out.println(arreyDiStringhe[i]);
-    }
+                if (arreyDiStringhe.get(i).equals( arreyDiStringhe.get(x))) {
+                    System.out.println("minchia, è uguale!");
+                }
+                System.out.println(arreyDiStringhe.get(i));
+            }
+            ;
 
+//            variabileDappoggio = arreyDiStringhe.get(i);
+//            if(variabileDappoggio == arreyDiStringhe.get(i + 1)){
+//
+//            }
 
+        }
 
 
     }

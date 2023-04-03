d93a1e102ba3f72cfb7160dd9d1cd8140e0552a3
RenDevon <amnesiaselettiva.mt@gmail.com>
2023-04-03 23:31:12 +0200

    stampare elementi di un array

--- src/mains.java
+++ src/mains.java
@@ -1,34 +1,19 @@
 public class mains {
 
     public static void main(String[] args) {
-        Guerriero conan = new Guerriero("Conan", 100, "Ascione");
-        Guerriero dartagnan = new Guerriero("Dartagnan", 100, "Ascione");
+    String occhio = ";scemo;beppe;scemo;enrico;mel;ytyty;";
+    occhio = occhio.substring(1);
+    String[] arreyDiStringhe = occhio.split(";");
 
-//        conan.getName();
-//        conan.cammina();
-//        dartagnan.cammina();
-//
-//        conan.attacca(dartagnan);
-//        conan.attacca(dartagnan);
-//        conan.attacca(dartagnan);
-//        conan.attacca(dartagnan);
-//        conan.attacca(dartagnan);
+        System.out.println(arreyDiStringhe.length);
+    for(int i = 0; i < arreyDiStringhe.length; i++ ) {
 
-        int numeroIntero = 432000000;
-        short numeroShort = 10000;
-        byte numeroByte = 100;
-        long numeroLong = 1L;
 
-        double numeroConVirgola = 1.23;
-        double uno = 1;
-        double due = 2;
-        double risultato = uno / due;
-        System.out.println(risultato);
-        float numerofloat = 1.23f;
+        System.out.println(arreyDiStringhe[i]);
+    }
 
-        String valoreAlfanumerico = "123!abc";
 
-        boolean sonoVeroOFalso = 0 == 0; // True
-        boolean sonoVeroOFalso2 = 0 != 0; // False
+
+
     }
 }

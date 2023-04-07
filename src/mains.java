518b5cf45784a5a864a4f8ce2908331e62ea4ded
RenDevon <amnesiaselettiva.mt@gmail.com>
2023-04-07 20:26:14 +0200

    escamotage

--- src/mains.java
+++ src/mains.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -5,31 +7,28 @@ import java.util.List;
 public class mains {
 
     public static void main(String[] args) {
-        String occhio = ";scemo;beppe;scemo;enrico;mel;ytyty;";
-        occhio = occhio.substring(1);
-        List<String> arreyDiStringhe = Arrays.asList(occhio.split(";"));
-        List<String> nuovoArray = new ArrayList<>(arreyDiStringhe);
-        nuovoArray.add("genoveffa");
-//        System.out.println(arreyDiStringhe.size());
-        String variabileDappoggio = "";
+        String gente = ";scemo;beppe;scemo;enrico;mel;";
+        gente = gente.substring(1);
+        List<String> arrayDiStringhe = Arrays.asList(gente.split(";"));
+        List<String> nuovoArray = new ArrayList<>(arrayDiStringhe);
+
+       List<String> robaDaLevare = nuovoArray;
+       robaDaLevare.remove("scemo");
+
+  //      String variabileDappoggio = "";
         for (int i = 0; i < nuovoArray.size(); i++) {
 
             for (int x = 0; x < nuovoArray.size(); x++) {
 
-                if (nuovoArray.get(i).equals( nuovoArray.get(x))) {
-                    System.out.println("minchia, è uguale!");
+                if (nuovoArray.get(i).equals(nuovoArray.get(x))){
+                    System.out.println(robaDaLevare.get(i));
+        //            System.out.println(arrayDiStringhe.get(i));
                 }
-                System.out.println(nuovoArray.get(i));
+
             }
-            ;
-
-//            variabileDappoggio = arreyDiStringhe.get(i);
-//            if(variabileDappoggio == arreyDiStringhe.get(i + 1)){
-//
-//            }
-
         }
 
 
     }
+
 }

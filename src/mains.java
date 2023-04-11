c19c12f37de488de9a538fceb9eae7449092e7c1
RenDevon <amnesiaselettiva.mt@gmail.com>
2023-04-11 13:02:37 +0200

    escamotage

--- src/mains.java
+++ src/mains.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import java.lang.reflect.Array;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,25 +10,15 @@ public class mains {
         String gente = ";scemo;beppe;scemo;enrico;mel;";
         gente = gente.substring(1);
         List<String> arrayDiStringhe = Arrays.asList(gente.split(";"));
-        List<String> nuovoArray = new ArrayList<>(arrayDiStringhe);
+        List<String> arrayDiStringheDivise = new ArrayList<>(arrayDiStringhe);
 
-       List<String> robaDaLevare = nuovoArray;
-       robaDaLevare.remove("scemo");
-
-  //      String variabileDappoggio = "";
-        for (int i = 0; i < nuovoArray.size(); i++) {
-
-            for (int x = 0; x < nuovoArray.size(); x++) {
-
-                if (nuovoArray.get(i).equals(nuovoArray.get(x))){
-                    System.out.println(robaDaLevare.get(i));
-        //            System.out.println(arrayDiStringhe.get(i));
-                }
-
+        List<String> stringhePulite = new ArrayList<>();
+        for (int i = 0; i < arrayDiStringheDivise.size(); i++) {
+            String elementoCorrente = arrayDiStringheDivise.get(i);
+            if (!stringhePulite.contains(elementoCorrente)) {
+                stringhePulite.add(elementoCorrente);
+                System.out.println(stringhePulite);
             }
         }
-
-
     }
-
 }
